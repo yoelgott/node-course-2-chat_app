3807ae4cb533e25dc069192a4e4b696c59f54504
yoel gottlieb <yoelgott@gmail.com>
2019-05-05 00:18:37 +0300

    socket-io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket_io = require('socket.io');
+
+const public_path = path.join(__dirname, '../public');
+var port = process.env.PORT || 3000;
 
 var app = express();
-const public_path = path.join(__dirname, '../public');
-
-var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socket_io(server);
 
 app.use(express.static(public_path));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected from server');
+    });
+});
+
+server.listen(port, () => {
     console.log(`open on port ${port}`);
 });
